c7cf9cbafae4eb2ce3c27eee889175178937b294
Maria Fernanda <mfraraujo@outlook.com>
2023-10-22 17:00:33 -0300

    fix: cors error

--- Program.cs
+++ Program.cs
@@ -2,25 +2,26 @@ using pokedex_api.Models;
 using pokedex_api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options => options.AddPolicy("AllowAnyOrigin", builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
 
-// Add services to the container.
 builder.Services.Configure<PokedexDatabaseSettings>(builder.Configuration.GetSection("PokedexDatabase"));
 
 builder.Services.AddSingleton<PokemonService>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowAnyOrigin");
 
 app.UseHttpsRedirection();
 
